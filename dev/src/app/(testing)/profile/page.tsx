99bb98aa7496a6c3d68a54af9dc687b0e92ec216
w41x <_@w41x.io>
2024-06-16 20:29:50 +0200

    some tweaks

--- dev/src/app/(testing)/profile/page.tsx
+++ dev/src/app/(testing)/profile/page.tsx
@@ -1,16 +1,18 @@
+'use client'
+
 import * as React from 'react'
 import Image from 'next/image.js'
-import {getServerSession} from 'next-auth'
+import {useSession} from 'next-auth/react'
 
 
-export default async function Page() {
+export default function Page() {
 
-    const session = await getServerSession()
+    const session = useSession()
 
-    const imageUrl = session?.user?.image
+    const imageUrl = session?.data?.user?.image
 
     return <>
-        <h1>{JSON.stringify(session)}</h1>
+        <h1>{JSON.stringify(session?.data)}</h1>
         {imageUrl ? <Image src={imageUrl} alt="Profile Picture" width={100} height={100}/> : <h1>No Avatar</h1>}
     </>
 
