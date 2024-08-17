c9522287c302c9ef9367c368767dfbe027185706
w41x <_@w41x.io>
2024-08-17 13:20:37 +0200

    updated to PayloadCMS v3.0.0-beta.84

---
+++ dev/src/app/(payload)/admin/importMap.js
@@ -0,0 +1,7 @@
+import { Avatar as Avatar_0 } from 'payload-zitadel-plugin/client'
+import { LoginButton as LoginButton_1 } from 'payload-zitadel-plugin/client'
+
+export const importMap = {
+  "payload-zitadel-plugin/client#Avatar": Avatar_0,
+  "payload-zitadel-plugin/client#LoginButton": LoginButton_1
+}
