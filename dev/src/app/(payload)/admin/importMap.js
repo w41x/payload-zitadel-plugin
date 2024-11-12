a922f0ce86397de0262883295c23a757217a912c
w41x <_@w41x.io>
2024-11-12 23:20:09 +0100

    Updated to PayloadCMS v3.0.0-beta.128

--- dev/src/app/(payload)/admin/importMap.js
+++ dev/src/app/(payload)/admin/importMap.js
@@ -1,7 +1,7 @@
 import { Avatar as Avatar_56c66942f17cf961f922b7f5275bedd4 } from 'payload-zitadel-plugin/components'
 import { LoginButton as LoginButton_56c66942f17cf961f922b7f5275bedd4 } from 'payload-zitadel-plugin/components'
-import { UserInfoCSR as UserInfoCSR_40c9b215273998a6c4c149ac686b2760 } from 'src/components'
-import { UserInfoSSR as UserInfoSSR_40c9b215273998a6c4c149ac686b2760 } from 'src/components'
+import { UserInfoCSR as UserInfoCSR_40c9b215273998a6c4c149ac686b2760 } from '/src/components'
+import { UserInfoSSR as UserInfoSSR_40c9b215273998a6c4c149ac686b2760 } from '/src/components'
 
 export const importMap = {
   "payload-zitadel-plugin/components#Avatar": Avatar_56c66942f17cf961f922b7f5275bedd4,
--- dev/src/app/(payload)/layout.tsx
+++ dev/src/app/(payload)/layout.tsx
@@ -2,13 +2,23 @@
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 
 import React, {PropsWithChildren} from 'react'
-import configPromise from '@payload-config'
-import {RootLayout} from '@payloadcms/next/layouts'
+import type {ServerFunctionClient} from 'payload'
+import config from '@payload-config'
+import {handleServerFunctions, RootLayout} from '@payloadcms/next/layouts'
 import {importMap} from './admin/importMap'
 
 import '@payloadcms/next/css'
 
+const serverFunction: ServerFunctionClient = async function (args) {
+    'use server'
+    return handleServerFunctions({
+        ...args,
+        config,
+        importMap
+    })
+}
+
 const Layout = ({children}: PropsWithChildren) =>
-    <RootLayout config={configPromise} importMap={importMap}>{children}</RootLayout>
+    <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>{children}</RootLayout>
 
 export default Layout
