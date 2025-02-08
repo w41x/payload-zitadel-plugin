b0646663d626aa4a1dde1b4f05d89fb4005c7632
w41x <_@w41x.io>
2025-02-08 16:50:37 +0100

    updated to PayloadCMS v3.22.0

--- dev/src/app/(testing)/layout.tsx
+++ dev/src/app/(testing)/layout.tsx
@@ -1,11 +1,19 @@
-import type {PropsWithChildren} from 'react'
+import React, {PropsWithChildren} from 'react'
+import type {Metadata} from 'next'
 
-export default async function Layout({children}: PropsWithChildren) {
+export const metadata: Metadata = {
+    title: 'Plugin Test',
+    description: 'Payload Zitadel Plugin'
+}
 
+export default async function RootLayout({children}: PropsWithChildren) {
     return (
+        <html>
+        <body>
         <main>
             {children}
         </main>
+        </body>
+        </html>
     )
-
 }
